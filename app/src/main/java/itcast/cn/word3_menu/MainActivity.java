d1031a9ae92cd5f7faf1fa47d0665affc26e66b0
Your Name <you@example.com>
2018-07-21 10:41:40 +0800

    Menu练习

---
+++ app/src/main/java/itcast/cn/word3_menu/MainActivity.java
@@ -0,0 +1,37 @@
+package itcast.cn.word3_menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"1",Toast.LENGTH_SHORT).show();
+                break;
+            case  R.id.remove_item:
+                Toast.makeText(this,"2",Toast.LENGTH_SHORT).show();
+                break;
+                default:break;
+        }
+        return true;
+    }
+}
